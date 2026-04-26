5b849dae5a87350e11339c23a0c9bb8590e289d1
Fedwa <fqlf123@gmail.com>
2026-04-26 15:48:53 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Reviews-project/';
+  base: '/Reviews-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
